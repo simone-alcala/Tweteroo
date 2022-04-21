5084b16d4844a72ea34e0698cd7003ebe1421a2a
simone-alcala <simone.alcala@gmail.com>
2022-04-21 01:19:38 -0300

    Added route /sign-up

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ import chalk   from 'chalk';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+const users  = [];
+//let tweet = {'username': '', 'avatar': '', 'tweet': ''};
+
+const tweets = [];
 
 app.get('/',(req,res) => {
   res.send('Foi :D :D');
-})
+});
+
+app.get('/tweets',(req,res) => {
+  res.send(tweets);
+});
+
+app.post('/sign-up',(req,res) => {
+  const {username,avatar} = req.body;
+  users.push({username,avatar});
+  res.send('OK');
+});
+
 
 app.listen(5000,()=>console.log(chalk.bold.green('Server on :D')));

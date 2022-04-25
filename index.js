55a7952b3a76ed686d0b4fd92b0ca22e2d0ecb2e
simone-alcala <simone.alcala@gmail.com>
2022-04-25 15:51:03 -0300

    Added paginated API

--- index.js
+++ index.js
@@ -10,16 +10,21 @@ const users  = [];
 const tweets = [];
 
 function validatePostSignUp(req,res,next){
-  if (!req.body.username) return res.status(400).json({error: "Username é obrigatório"});
-  if (!req.body.avatar) return res.status(400).json({error: "Avatar é obrigatório"});
-  if (!validateURL(req.body.avatar)) return res.status(400).json({error: "Avatar inválido"});
+  if (!req.body.username) return res.status(400).json({error: 'Username é obrigatório'});
+  if (!req.body.avatar) return res.status(400).json({error: 'Avatar é obrigatório'});
+  if (!validateURL(req.body.avatar)) return res.status(400).json({error: 'Avatar inválido'});
   return next();
 }
 
 function validatePostTweet(req,res,next){
-  if (!req.headers.user) return res.status(400).json({error: "Username é obrigatório"});
-  if (!req.body.tweet) return res.status(400).json({error: "Tweet é obrigatório"});
-  if (!users.find(user => user.username === req.headers.user)) return res.status(400).json({error: "Username inválido"});
+  if (!req.headers.user) return res.status(400).json({error: 'Username é obrigatório'});
+  if (!req.body.tweet) return res.status(400).json({error: 'Tweet é obrigatório'});
+  if (!users.find(user => user.username === req.headers.user)) return res.status(400).json({error: 'Username inválido'});
+  return next();
+}
+
+function validateGetTweet(req,res,next){
+  if (!req.query.page > 1) return res.status(400).json({error: 'Informe uma página válida!'});
   return next();
 }
 
@@ -29,17 +34,33 @@ function validateURL(url){
 }
 
 app.get('/',(req,res) => {
-  res.send('Foi :D :D');
+  res.send('Curioso(a) :D :D');
 }); 
 
+
+app.get('/tweets',validateGetTweet,(req,res) => {
+  const page = req.query.page;
+  const limit = 10;//req.query.limit;
+
+  console.log(req.query.limit);
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const tweetsToReturn = tweets.slice(startIndex,endIndex);
+  res.json(tweetsToReturn);
+
+});
+/*
 app.get('/tweets',(req,res) => {
-  const lastTweets = (tweets.slice()).splice(-2);
+  const lastTweets = (tweets.slice()).splice(-10);
   lastTweets.map(tweet => {
     const user = users.find(u => u.username === tweet.username);
     tweet.avatar = user.avatar;
   });
   res.send(lastTweets);
 });
+*/
 
 app.get('/tweets/:username',(req,res) => {
   const username = req.params.username;
@@ -56,8 +77,16 @@ app.post('/sign-up',validatePostSignUp,(req,res) => {
 app.post('/tweets',validatePostTweet,(req,res) => {
   const tweet = req.body.tweet ;
   const username = req.headers.user;
-  tweets.push({username,tweet});
+
+  const user = users.find(u => u.username === username);
+  
+  tweets.push({
+    username: username,
+    avatar: typeof(user)==='undefined'?null:user.avatar,
+    tweet: tweet });
+
   res.status(201).send('OK');
+  
 });
 
 app.listen(5000,()=>console.log(chalk.bold.green('Server on :D')));
